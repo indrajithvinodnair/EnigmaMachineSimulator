c29f3d8a38e60590785f3a88145e1caec453366d
cptblackbeard <cptBlackBeard>
2024-07-30 07:29:33 +0530

    Modified CORS settings

--- Backend/src/main/java/com/wehrmacht/enigma/config/WebConfig.java
+++ Backend/src/main/java/com/wehrmacht/enigma/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.wehrmacht.enigma.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // Adjust the origin as needed
+                .allowedOrigins("http://localhost") // Adjust the origin as needed
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
